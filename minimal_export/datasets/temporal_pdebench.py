51d628772a6c7aa80232dbe9fbbed3df5ac7fa90
muzhaoyang <1748492875@qq.com>
2026-03-28 00:36:05 +0800

    chore: remove minimal_export from parent repo as it will be an independent repo

--- minimal_export/datasets/temporal_pdebench.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .pdebench import PDEBenchDataModule as TemporalPDEBenchDataModule
-
-__all__ = ["TemporalPDEBenchDataModule"]
--- minimal_export/models/swin_unet.py
+++
@@ -1,3 +0,0 @@
-from .spatial.swin_unet import SwinUNet
-
-__all__ = ["SwinUNet"]
